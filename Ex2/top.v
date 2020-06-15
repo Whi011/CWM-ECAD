7dda2a737f156db388a66bcff64a6fe2a34a1d93
widojoman <matthewwidojo.MAW@gmail.com>
2020-06-15 15:54:48 +0000

    Sill Errors

--- Ex2/top.v
+++ Ex2/top.v
@@ -22,11 +22,26 @@
 
 module mux(
     //Todo: define inputs here
+	input A,
+	input B,
+	input sel,
+	output out
     );
     
     //Todo: define registers and wires here
+	wire = out
+	
 
+	parameter CLK_PERIOD = 10; //set the clock period
+		initial
+			begin
+				parameter clk = 1'b0;
+				forever
+					#(CLK_PERIOD/2) clk=~clk; //happens every delay of CLK_PERIOD/2
+			end
 
-    //Todo: define your logic here                 
-      
+    //Todo: define your logic here    
+	always @(posedge clk) begin
+		assign out = (a & !sel) | (b & sel)             
+      	end
 endmodule
