cf4b0a4661a0b25dc4e057dcecd8c3d7d7027152
widojoman <matthewwidojo.MAW@gmail.com>
2020-06-16 15:41:31 +0000

    Still writing

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
-// Exercise #4 
-// Student Name:
-// Date: 
+// Exercise #4
+// Student Name: Roland Whiteley
+// Date: 16-06-2020
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
@@ -15,4 +15,18 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module counter(
+    //Todo: add ports 
+  input clk,	// reset
+  input rst,	// enable 
+  input button,	// direction
+  output reg [2:0] throw	// counter out
+    );
+
+  always @ (posedge clk)
+  begin
+    
+  end
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,47 @@
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+   //No ports are defined! 
+   );
+
+   
+    //Parameters    
+    parameter CLK_PERIOD = 10;
+
+    //Registers
+    reg clk;
+    reg rst;
+    reg button;
+    reg [2:0] throw;
+
+    //Wires
+    wire out;
+    
+
+    //An initial block that creates the clock
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+    
+     //An initial block that drives the logic and tests output
+
+     //An initial block that ends the simulation, and declares success (if valid)
+     
+     //The instantiation of the user's module
+
+     dice top (
+     .clk (clk),
+     .rst (rst),
+     .button (button),
+     .throw (out)
+     );
+     
+
+     
+endmodule
