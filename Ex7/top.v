cc5528932ea06b28d065629984e83c3dbe5f45d2
widojoman <matthewwidojo.MAW@gmail.com>
2020-06-19 16:00:41 +0000

    Still working on it

--- Ex7/top.v
+++ Ex7/top.v
@@ -12,5 +12,29 @@
 //  outputs:
 //           result[4:0]
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
 
+module times_tb(
+  input clk,
+  input [2:0] a,
+  input [2:0] b,
+  input enable,
+  output [5:0] result
+  );
+
+  wire [5:0] ab;
+
+  assign ab = {a,b};
+
+
+//  mybram lookup_table (
+//  .clka(clk),     // input wire clka
+//  .ena(enable),   // input wire ena
+//  .wea(0),      // input wire [0 : 0] wea
+//  .addra(ab),     // input wire [5 : 0] addra
+//  .dina(result),    // input wire [5 : 0] dina
+//  .douta(result)  // output wire [4 : 0] douta
+//);
+	
+endmodule
 
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -6,4 +6,33 @@
 // Description: A testbench module to test Ex7 - Times Table
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
 
+module times_table_tester();
+
+  parameter CLK_PERIOD = 10;
+  reg clk;
+  reg enable;
+  reg err;
+  reg [2:0] a;
+  reg [2:0] b;
+  
+  wire [5:0] result;
+
+
+  initial begin
+  clk = 0;
+  forever
+    #(CLK_PERIOD/2) clk = ~clk;
+  end
+
+  times_tb top (
+  .clk (clk),
+  .a (a),
+  .b (b),
+  .enable (enable),
+  .result (result)
+  );
+
+
+endmodule
